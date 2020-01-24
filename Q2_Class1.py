be392c30d4c314aa6221de5958c3633ee9229b9b
Geetanjali Makineni <59195740+geetamakineni@users.noreply.github.com>
2020-01-24 13:05:19 -0600

    Add files via upload

---
+++ Q2_Class1.py
@@ -0,0 +1,5 @@
+String_name=str(input("Given String:"))
+length_string=len(String_name)
+String_name=String_name[2:length_string]
+String_name = String_name[::-1];
+print(String_name)
---
+++ Q3_Class1.py
@@ -0,0 +1,3 @@
+given_string=str(input("Enter String"))
+final_string=given_string.replace("python","pythons")
+print(final_string)
